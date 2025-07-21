4c41312f70d2f3ed87f3570b1446f3c55227575b
santuflow <rodrigosantua2@gmail.com>
2025-07-21 04:53:31 -0300

    Fix registro: método pbkdf2:sha256

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     balances = db.relationship('Balance', backref='user', lazy=True)
 
 
@@ -108,7 +108,7 @@ def register():
         return jsonify(success=False, message='Email registrado.'), 409
     user = User(
         username=u, email=e,
-        password_hash=generate_password_hash(p, method='sha256')
+        password_hash=generate_password_hash(p, method='pbkdf2:sha256', salt_length=8)
     )
     db.session.add(user)
     db.session.commit()
