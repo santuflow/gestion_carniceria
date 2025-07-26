4013be8d813165ead2eb6a7be7e4f8eb83b8a341
santuflow <rodrigosantua2@gmail.com>
2025-07-26 16:52:26 -0300

    Fix nombre no se mostraba en el CV

--- app.py
+++ app.py
@@ -685,11 +685,10 @@ google = oauth.register(
     name='google',
     client_id=os.environ.get('GOOGLE_CLIENT_ID'),
     client_secret=os.environ.get('GOOGLE_CLIENT_SECRET'),
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    client_kwargs={'scope': 'openid profile email'}
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={'scope': 'openid email profile'}
 )
+
 
 from flask import session
 
